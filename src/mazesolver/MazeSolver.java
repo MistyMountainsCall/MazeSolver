6326f7dee28cdb3febac31da27268e0fb267fe80
ForestShade <ForestShade@ForestShade-PC>
2016-03-22 12:00:00 -0700

    Maze solving algorithm initial commit.

---
+++ src/mazesolver/MazeSolver.java
@@ -0,0 +1,20 @@
+package mazesolver;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Walker VanHouten
+ */
+public class MazeSolver {
+
+    /**
+     * @param args the command line arguments
+     * filename of maze file
+     */
+    public static void main(String[] args) throws IOException {
+        Maze m = new Maze(args[1]);
+        
+        
+    }
+}
